af3847816645c49d0695ab2600d41cb1ecb42b4b
osmandlsmn <dlsmnosman@gmail.com>
2024-04-27 21:42:25 +0300

    feat: add seo metadata

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,12 @@
 import ProjectsHeader from "@/components/(projects)/projects-header";
 import ProjectsList from "@/components/(projects)/projects-list";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ali Osman Delişmen - Projects",
+  description:
+    "Ali Osman Delişmen has been working on software development in Istanbul since 2019. While developing his frontend skills with open source projects, he continues to increase his expertise in DevOps in his free time.",
+};
 
 export default function ProjectsPage() {
   return (
